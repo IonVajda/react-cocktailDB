80bbe859eba8f1e57f69fa736203948b87256d95
Ion Vajda <v.ionut24@gmail.com>
2020-09-13 19:38:41 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+        recusandae rerum necessitatibus ullam, minus dicta. Quibusdam quas nulla
+        ducimus dolor magnam rerum illo fuga, animi ad excepturi quis quidem
+        suscipit!
+      </p>
+    </section>
+  );
+}
